bc117f6866abd1675553853902d6996e40ddf0c5
smail19 <50032979+smail19@users.noreply.github.com>
2020-12-07 17:52:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
+/*document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#81B622';//#29323c
 	} else {
-		header.style.backgroundColor = 'transparent';
+		header.style.backgroundColor = '#81B622';
 	}
-});
+});*/
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
